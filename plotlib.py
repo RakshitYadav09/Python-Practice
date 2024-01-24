c3df1476466b646d3fa0f2c4dea216313d5abeed
Rakshit <rakshityadav411@gmail.com>
2024-01-24 23:08:32 +0530

    graphs!!!

--- plotlib.py
+++ plotlib.py
@@ -1,2 +1,12 @@
+import numpy as np
 import matplotlib.pyplot as plt 
-plt.style.use('dark_backgroud')
+print(np.pi)
+print(np.tan(np.pi/2))
+##using matplotlib with numpy
+
+x=np.arange(1,11)
+y=np.arange(10,110,10)
+
+plt.figure(figsize=(6,6))
+plt.plot(x,y,'r--')
+plt.show()
