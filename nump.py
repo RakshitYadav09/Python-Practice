2f940bb5de3fb4005bb4a925585ea430a76d75fe
Rakshit <rakshityadav411@gmail.com>
2024-01-24 22:42:28 +0530

    removed commenting

--- nump.py
+++ nump.py
@@ -3,15 +3,15 @@ import sys
 a=np.array([2,3,4,5,3,2,6])
 d=np.array([[1,2,3],
            [2,3,4]])
-'''print(d)
+print(d)
 print(d.dtype)
 print(d.size)
 print(a[4]-d[1])
 print(d[1,-1])
-print(d.ndim)'''
-'''b=np.empty((4,4), dtype=float)
+print(d.ndim)
+b=np.empty((4,4), dtype=float)
 c=np.ones((6,5), dtype=int)
 e=np.ones((4,4), dtype=str)
 print(b)
 print(c)
-print(e)'''
+print(e)
